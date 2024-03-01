52092a4d70f22d7f0a5c8d4831ce2b9e55ba2f94
Dainovskiene <neringa.dainovskiene@accenture.com>
2024-03-01 16:38:48 +0200

    Implement http client

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from "@angular/core";
+import { DataStorageService } from "../shared/data-storage.service";
+import { Recipe } from "../recipes/recipe.model";
+import { RecipeService } from "../recipes/recipe.service";
 
 @Component({
   selector: 'app-header',
@@ -7,4 +10,15 @@ import { Component } from "@angular/core";
 })
 export class HeaderComponent {
   collapsed = true;
+  recipes: Recipe[];
+
+  constructor(private dataStorageService: DataStorageService, private recipeService: RecipeService) {}
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
+  }
+
+  onFetchData() {
+    this.dataStorageService.fetchRecipes().subscribe();
+  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,61 +7,61 @@ import { Subject } from "rxjs";
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
-    
-    private recipes: Recipe[] = [
-        new Recipe(
-          'Apple pie',
-          'A tasty heartwarming pie.',
-          '../../assets/images/applePie.jpg',
-          [
-            new Ingredient('Apple', 5),
-            new Ingredient('Flour', 1),
-            new Ingredient('Sugar', 1),
-            new Ingredient('Butter', 1),
-            new Ingredient('Milk', 1)
-          ]
-        ),
-        new Recipe(
-          'Pumkin pie',
-          'A smooth and spicy pie.',
-          '../../assets/images/pumpkinPie.webp',
-          [
-            new Ingredient('Pumpkin', 1),
-            new Ingredient('Flour', 1),
-            new Ingredient('Sugar', 1),
-            new Ingredient('Oil', 1),
-            new Ingredient('Spices', 6)
-          ]
-        ),
-        new Recipe(
-          'Cherry pie',
-          'This classic cherry pie will be your favorite!',
-          '../../assets/images/cherryPie.jpeg',
-          [
-            new Ingredient('Cherries ', 20),
-            new Ingredient('Flour', 1),
-            new Ingredient('Sugar', 1),
-            new Ingredient('Butter ', 1),
-            new Ingredient('Lemon', 1),
-            new Ingredient('Vanilla', 1),
-            new Ingredient('Salt', 1)
-          ]
-        ),
-        new Recipe(
-          'Lemon meringue pie',
-          'This is the perfect lemon meringue pie!',
-          '../../assets/images/lemonMeringuePie.jpeg',
-          [
-            new Ingredient('Egg ', 5),
-            new Ingredient('Cornstarch', 1),
-            new Ingredient('Sugar', 1),
-            new Ingredient('Butter ', 1),
-            new Ingredient('Lemon', 1),
-            new Ingredient('Cream', 1),
-            new Ingredient('Salt', 1)
-          ]
-        ),
-    ];
+  private recipes: Recipe[] = [];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //       'Apple pie',
+    //       'A tasty heartwarming pie.',
+    //       '../../assets/images/applePie.jpg',
+    //       [
+    //         new Ingredient('Apple', 5),
+    //         new Ingredient('Flour', 1),
+    //         new Ingredient('Sugar', 1),
+    //         new Ingredient('Butter', 1),
+    //         new Ingredient('Milk', 1)
+    //       ]
+    //     ),
+    //     new Recipe(
+    //       'Pumkin pie',
+    //       'A smooth and spicy pie.',
+    //       '../../assets/images/pumpkinPie.webp',
+    //       [
+    //         new Ingredient('Pumpkin', 1),
+    //         new Ingredient('Flour', 1),
+    //         new Ingredient('Sugar', 1),
+    //         new Ingredient('Oil', 1),
+    //         new Ingredient('Spices', 6)
+    //       ]
+    //     ),
+    //     new Recipe(
+    //       'Cherry pie',
+    //       'This classic cherry pie will be your favorite!',
+    //       '../../assets/images/cherryPie.jpeg',
+    //       [
+    //         new Ingredient('Cherries ', 20),
+    //         new Ingredient('Flour', 1),
+    //         new Ingredient('Sugar', 1),
+    //         new Ingredient('Butter ', 1),
+    //         new Ingredient('Lemon', 1),
+    //         new Ingredient('Vanilla', 1),
+    //         new Ingredient('Salt', 1)
+    //       ]
+    //     ),
+    //     new Recipe(
+    //       'Lemon meringue pie',
+    //       'This is the perfect lemon meringue pie!',
+    //       '../../assets/images/lemonMeringuePie.jpeg',
+    //       [
+    //         new Ingredient('Egg ', 5),
+    //         new Ingredient('Cornstarch', 1),
+    //         new Ingredient('Sugar', 1),
+    //         new Ingredient('Butter ', 1),
+    //         new Ingredient('Lemon', 1),
+    //         new Ingredient('Cream', 1),
+    //         new Ingredient('Salt', 1)
+    //       ]
+    //     ),
+    // ];
 
     constructor(private shoppingListService: ShoppingListService) {}
 
@@ -94,5 +94,10 @@ export class RecipeService {
 
     informOnChanges() {
       this.recipesChanged.next(this.getRecipes());
+    }
+
+    setRecipes(recipes: Recipe[]) {
+      this.recipes = recipes;
+      this.informOnChanges();
     }
 }
