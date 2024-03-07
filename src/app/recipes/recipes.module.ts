6a12cf3e43f399e9e2c387de0ca99777ca7febab
Dainovskiene <neringa.dainovskiene@accenture.com>
2024-03-07 12:46:29 +0200

    Split Auth, Shared, Core modules; Implement lazy loading

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+
 import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
 import { RecipeListComponent } from "./recipe-list/recipe-list.component";
 import { RecipesComponent } from "./recipes.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
 import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
-import { RouterModule } from "@angular/router";
-import { CommonModule } from "@angular/common";
-import { ReactiveFormsModule } from "@angular/forms";
 import { RecipesRoutingModule } from "./recipes-routing.module";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
     declarations: [
@@ -21,8 +21,8 @@ import { RecipesRoutingModule } from "./recipes-routing.module";
     ],
     imports: [
         RecipesRoutingModule,
-        CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        SharedModule
     ],
     exports: [
         // RecipesComponent,
--- src/app/shopping/shopping-list/shopping-list.module.ts
+++ src/app/shopping/shopping-list/shopping-list.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from "@angular/core";
 import { ShoppingListEditComponent } from "./shopping-list-edit/shopping-list-edit.component";
 import { ShoppingListComponent } from "./shopping-list.component";
-import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { ShoppingListRoutingModule } from "./shopping-list-routing.module";
+import { SharedModule } from "src/app/shared/shared.module";
 
 @NgModule({
     declarations: [
@@ -11,7 +11,7 @@ import { ShoppingListRoutingModule } from "./shopping-list-routing.module";
         ShoppingListEditComponent
     ],
     imports: [
-        CommonModule,
+        SharedModule,
         FormsModule,
         ShoppingListRoutingModule
     ]
